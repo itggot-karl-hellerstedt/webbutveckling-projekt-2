7b8b30dcf70dd1c6401644820a245f55d12724d4
itggot-karl-hellerstedt <43438073+itggot-karl-hellerstedt@users.noreply.github.com>
2019-01-28 13:58:34 +0100

    added javascript file and used it in about.html

---
+++ site/js/main.js
@@ -0,0 +1,24 @@
+function visaHjul(event){
+    let dis = document.querySelector("#ostronkorvX")
+    dis.classList.toggle("dis")
+}
+
+function visaHjultvå(event){
+    let dis = document.querySelector("#ostronkorvXX")
+    dis.classList.toggle("dis")
+}
+
+function visaHjultre(event){
+    let dis = document.querySelector("#ostronkorvXXX")
+    dis.classList.toggle("dis")
+}
+
+function visaHjulfyra(event){
+    let dis = document.querySelector("#ostronkorvXXXX")
+    dis.classList.toggle("dis")
+}
+
+function visaHjulfem(event){
+    let dis = document.querySelector("#ostronkorvXXXX")
+    dis.classList.toggle("dis")
+}
